d3fcde0d05a39ef7255579a9d423a6a3ce82069e
bernie-haxx <benkaranja43@gmail.com>
2025-05-05 00:53:55 +0300

    chore:redo
    It was shit nvm

--- tmux/scripts/mac-player.sh
+++ tmux/scripts/mac-player.sh
@@ -5,31 +5,12 @@
 export LC_ALL=en_US.UTF-8
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$current_dir/utils.sh" || { echo "❌ utils.sh missing"; exit 1; }
+source $current_dir/utils.sh
 
 if [[ "$OSTYPE" != "darwin"* ]]; then
     echo "⏭️ Spotify (macOS only)"
-    exit 0
+    exit 1
 fi
-
-function slice_loop() {
-
-	local str="$1"
-	local start="$2"
-	local how_many="$3"
-	local len=${#str}
-
-	local result=""
-
-	for ((i = 0; i < how_many; i++)); do
-		local index=$(((start + i) % len))
-		local char="${str:index:1}"
-		result="$result$char"
-	done
-
-	echo "$result"
-
-}
 
 get_spotify_status() {
     local spotify_data
@@ -54,37 +35,14 @@ END
 
 main() {
     local cache_file="/tmp/tmux_spotify_cache"
-    local rate=$(get_tmux_option "@dracula-refresh-rate" 5)
+    RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
 
-    if [ ! -f "$cache_file" ] || [ $(($(date +%s) - $(stat -f%c "$cache_file"))) -ge "$rate" ]; then
+    if [ ! -f "$cache_file" ] || [ $(($(date +%s) - $(stat -f%c "$cache_file"))) -ge "$RATE" ]; then
         get_spotify_status > "$cache_file"
     fi
-    # cat "$cache_file"
+    cat "$cache_file"
 
 
-    spotify_status=$(get_spotify_status)
-    start=0
-    len=${#spotify_status}
-    terminal_width=25
-
-      if [[ $len == 1 ]]; then
-        exit
-      fi
-
-      scrolling_text=""
-
-      for ((start = 0; start <= len; start++)); do
-        scrolling_text=$(slice_loop "$spotify_status" "$start" "$terminal_width")
-        echo -ne "\r"
-        echo "$scrolling_text "
-        echo -ne "\r"
-
-        sleep 0.08
-      done
-
-      echo -ne "\r"
-      echo "$scrolling_text "
-      echo -ne "\r"
 }
 
 main "$@"
