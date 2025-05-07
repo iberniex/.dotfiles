1c472267e429fc6c252bdefbc412006da34f24ad
bernie-haxx <benkaranja43@gmail.com>
2025-05-07 20:49:17 +0300

    fix(image):bug fix

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -1,8 +1,7 @@
 return {
   "3rd/image.nvim",
-  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-  opts = {
-    backend = "kitty",
-    processor = "magick_cli",
+  build = false,
+  opt = {
+    backend = "Kitty",
   },
 }
