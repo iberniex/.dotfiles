640bd1b937117645c8c6f32e0333fac938909945
bernie-haxx <benkaranja43@gmail.com>
2025-05-09 12:27:06 +0300

    fix: theme tweaks

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -7,11 +7,11 @@ return {
       terminal_colors = true,
       transparent_mode = true,
       italic = {
-        strings = true,
-        emphasis = true,
+        strings = false,
+        emphasis = false,
         comments = true,
-        operators = true,
-        folds = true,
+        operators = false,
+        folds = false,
       },
     })
     vim.o.background = "dark", vim.cmd([[colorscheme gruvbox]])
