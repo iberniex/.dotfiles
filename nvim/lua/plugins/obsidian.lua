bf5e7bd53c72928b10b0f6d2fac72ee99dbf8858
bernie-haxx <benkaranja43@gmail.com>
2025-05-11 17:33:00 +0300

    feat(frontmatter): title configuration

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -104,6 +104,31 @@ return {
       end
       return randm .. "-" .. tostring(os.date("%Y%m%d")) .. "-" .. suffix
     end,
+
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+
+    -- customize how wiki links are formatted
+    wiki_link_func = "use_alias_only",
+
     -- Optional, for templates (see below).
     templates = {
       folder = "~/.config/zk/templates",
