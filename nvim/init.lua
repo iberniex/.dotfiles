bb750d11f35429b273daf19824431b6bf4b1dbd8
bernie-haxx <benkaranja43@gmail.com>
2025-05-15 21:20:45 +0300

    feat(telescope): cleanup keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,27 +11,6 @@ vim.o.shiftwidth = 4
 -- Lazy configuration
 require("config.lazy")
 
--- Telescope configuration
-
-require("telescope").setup({
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-    },
-  },
-})
-
--- telescope-fxf-ext loading_extension
-require("telescope").load_extension("fzf")
-
--- telescope keybinds
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
 -- Text LSPConfig hover on buffer change
 vim.diagnostic.config({
   virtual_text = true,
--- nvim/lua/plugins/nvim-telescope.lua
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -4,7 +4,28 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependences = { "nvim-lua/plenary.nvim" },
-    config = true,
+    config = function()
+      -- Telescope configuration
+
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+        },
+      })
+
+      -- telescope-fxf-ext loading_extension
+      require("telescope").load_extension("fzf")
+
+      -- telescope keybinds
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", { desc = "telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "telescope help tags" })
+    end,
   },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 }
