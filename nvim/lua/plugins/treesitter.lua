d83d1b416a8b39cab716182eacf8d54014e4d21d
bernie-haxx <benkaranja43@gmail.com>
2025-05-16 13:43:08 +0300

    fix: solve treesitter error

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
 
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        enable = true,
+      })
+    end,
+  },
 }
