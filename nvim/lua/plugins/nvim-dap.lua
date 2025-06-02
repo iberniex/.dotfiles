c38fb4805e5e0751599559b47ad1180d1e5dc796
bernie-haxx <benkaranja43@gmail.com>
2025-06-02 16:32:27 +0300

    feat(nvim-dap): initialization and configuration

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,3 +1,27 @@
 return {
   "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    -- lldb configuration
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "/usr/bin/lldb",
+      name = "lldb",
+    }
+
+    dap.configurations.cpp = {
+      name = "Launch",
+      type = "lldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+  end,
 }
