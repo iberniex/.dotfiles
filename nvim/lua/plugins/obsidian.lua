58c2b5480c5021f725e3f3502087663b82182228
iberniex <ben.karanja@icloud.com>
2025-06-12 12:27:12 +0300

    feat: obsidian daily notes

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -52,6 +52,19 @@ return {
     -- default folder for notes
     notes_subdir = "000-Index",
 
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "~/Documents/vault/002-Journal",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+
     -- auto completion with cmp
     completion = {
       nvim_cmp = true,
