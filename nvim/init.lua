ccb449583fa23911d9647986306902c900152d76
iberniex <ben.karanja@icloud.com>
2025-06-26 22:23:17 +0300

    feat: man pages configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,5 +13,16 @@ vim.diagnostic.config({
   update_in_insert = false,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "man",
+  callback = function()
+    vim.cmd([[
+      hi link manSection Title
+      hi link manSubHeading Identifier
+      hi link manBold Function
+      hi link manUnderline Type
+    ]])
+  end,
+})
 -- Lazy configuration
 require("config.lazy")
