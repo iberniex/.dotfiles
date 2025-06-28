b1a8aaa532aff7c257da4680ce10fa29f33f560d
iberniex <ben.karanja@icloud.com>
2025-06-28 14:40:04 +0300

    chore: stylua formatting

--- nvim/lua/plugins/bridge.lua
+++ nvim/lua/plugins/bridge.lua
@@ -1,14 +1,13 @@
 local bridge_settings = {
-	obsidian_server_address = "https://127.0.0.1:27124",
-	scroll_sync = false,
-	cert_path = "~/.ssl/obsidian.crt",
-	warnings = true,
-	picker = "telescope",
+  obsidian_server_address = "https://127.0.0.1:27124",
+  scroll_sync = false,
+  cert_path = "~/.ssl/obsidian.crt",
+  warnings = true,
+  picker = "telescope",
 }
 
-
 return {
-	"oflisback/obsidian-bridge.nvim",
+  "oflisback/obsidian-bridge.nvim",
   dependencies = { "nvim-telescope/telescope.nvim" },
   -- dependencies = { "ibhagwan/fzf-lua" }, -- For picker = "fzf_lua"
   opts = bridge_settings,
@@ -18,4 +17,3 @@ return {
   },
   lazy = false,
 }
-
