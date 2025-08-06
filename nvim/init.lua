4d565543f0b27053c9c4f419a9c781117af12387
iberniex <ben.karanja@icloud.com>
2025-08-06 21:53:17 +0300

    chore: move keybinds to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,38 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.o.syntax = "enable"
 
+-- custom keymaps
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+-- escape configuration to change from ESC to "jj"
+if vim.g.vscode then
+  vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+end
+
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+-- save configuration
+-- single save configuration
+vim.keymap.set("n", "<leader>ww", ":w<CR>", { noremap = true, silent = true })
+
+-- save and quit configuration
+vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true, silent = true, desc = "save and quit" })
+
+-- save all buffers configuration
+vim.keymap.set(
+  "n",
+  "<leader>wa",
+  ":wa<CR>",
+  { noremap = true, silent = true, desc = "save all files in buffer" }
+)
+
+-- quit configuration
+vim.keymap.set("n", "<leader>qq", ":q<CR>", { noremap = true, silent = true, desc = "quit" })
+
+vim.keymap.set("n", "<leader>c", function()
+  require("mini.bufremove").delete(0, false)
+end)
 -- Text LSPConfig hover on buffer change
 vim.diagnostic.config({
   virtual_text = true,
